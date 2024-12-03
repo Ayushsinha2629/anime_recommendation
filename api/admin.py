6afcd0bbf5aa04059026e5841186b09f06b5c9ab
Ayushsinha2629 <ayush61187@gmail.com>
2024-12-04 01:51:02 +0530

    Added Admin.py files to access backend from django admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import User, UserPreference, CachedAnime
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'is_staff', 'is_active']
+
+@admin.register(UserPreference)
+class UserPreferenceAdmin(admin.ModelAdmin):
+    list_display = ['user', 'favorite_genres', 'created_at', 'updated_at']
+
+@admin.register(CachedAnime)
+class CachedAnimeAdmin(admin.ModelAdmin):
+    list_display = ['anime_id', 'title_romaji', 'title_english', 'popularity', 'cached_at']
